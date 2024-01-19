dec57e03c8c24f3961db5412c6dd6695addc2606
Robin Larsson <larssonrobin88@gmail.com>
2024-01-19 18:30:55 +0100

    While Snares

--- Starter.py
+++ Starter.py
@@ -19,34 +19,60 @@ def post_json(url, body):
     req = requests.post(url, json = body, headers=Headers)
     return req.text
 
-def game_solver(game):
-    best_action = {}
-    game_actions = game["actions"]
-    # Sort and use the action that gives biggest multiplyer
-    for action in game_actions:
-        if best_action == {}:
-            best_action = action
-        elif best_action["pointMultiplyer"] < action["pointMultiplyer"]:
-            best_action = action
-    print("Best action: " + str(best_action))
+# def game_solver(game):
+    # best_action = {}
+    # game_actions = game["actions"]
+    # # Sort and use the action that gives biggest multiplyer
+    # for action in game_actions:
+    #     if best_action == {}:
+    #         best_action = action
+    #     elif best_action["pointMultiplyer"] < action["pointMultiplyer"]:
+    #         best_action = action
+    # print("Best action: " + str(best_action))
+    
+    
+    ##Testa en aktivitet
 
-    actionActivityList = []
-    # Uses the best action on so many activities we can without going over the energy limit
-    actionActivityObject = {
-        "action": best_action["action"],
-        "activity": game["activitys"][0]["activity"]
-    }
-    actionActivityList.append(actionActivityObject)
+    
+    # actionActivityList = []
+    # # Uses the best action on so many activities we can without going over the energy limit
+    # actionActivityObject = {
+    #     "action": best_action["action"],
+    #     "activity": game["activitys"][0]["activity"]
+    # }
+    # actionActivityList.append(actionActivityObject)
 
     return actionActivityList
+
+def game_solver(game):
+    selected_action = game["actions"][0]
+    total_energy = game["maxEnergy"]
+    activityNumber = 0
+    actionActivityList = []
     
+    ## BARA SNARES
+    while total_energy >= selected_action["energyCost"]:
+        # Check if activityNumber is within the valid range
+        if activityNumber < len(game["activitys"]):
+            action_activity_object = {
+                "action": selected_action["action"], 
+                "activity": game["activitys"][activityNumber]["activity"]
+            }
+            actionActivityList.append(action_activity_object)
+            total_energy -= selected_action["energyCost"]
+            activityNumber += 1
+        else:
+            break  # Exit the loop if activityNumber is out of range
+
+    return actionActivityList
+
 
 # Fetch game and print out the Json
 
 game = fetch_json(baseURL + "/game")
 print("Game:")
 print(game)
-print("----")
+print("---------------------------------------------------------------------")
 
 # A solution that just try to take the best action and do as many tasks it can
 solution = game_solver(game)
@@ -57,4 +83,5 @@ body = {
 }
 print(body)
 score = post_json(baseURL + "/game/v2/submit/", body)
-print(score)
+print("Your score is: " + score)
+
